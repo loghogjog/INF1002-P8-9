ff012850fe79903348f2105fea7ec73d0efa5417
loghogjog <tantris.tan345@gmail.com>
2025-09-10 17:14:10 +0800

    added basic skeleton of project

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# PhishGuard
+## Project README
+Things to do after cloning the repository
+---
+
+## SETUP
+```bash
+1. Install tailwind CSS standalone CLI from https://github.com/tailwindlabs/tailwindcss/releases/tag/v4.1.13 (if you download to project directory, make sure to rename to tailwindcss)
+2. pip install -r requirements.txt
+3. cd to your project directory
+4. <PATH_TO_EXECUTABLE> -i src/input.css -o src/output.css --watch
+5. python app.py
+```
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -6,14 +6,37 @@ app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limit upload size to 16MB (TBC)
 
 ALLOWED_EXTENSIONS = {".eml"}
+FILENAME = ""
+list_of_reasons = []
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html', reasons=list_of_reasons)
+
 @app.route('/upload', methods=['POST'])
 def upload_eml_file():
     if 'file' not in request.files:
-        return "No file part", 400
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        # insert filename hashing here
+        filename = secure_filename(file.filename)
+        file.save(app.config['UPLOAD_FOLDER'] + filename)
+        global FILENAME
+        FILENAME = filename
+        # process the file and update list_of_reasons
+        # call custom functions here
+        return redirect(url_for('home'))
+
+    
+    
     
 
 
